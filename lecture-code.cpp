df6d951425fa6743803a96e7051c7cc61dbdd27b
Carlos R. Arias <arias@spu.edu>
2020-05-01 10:15:38 -0700

    with command line parameters and dynamically allocated array

--- lecture-code.cpp
+++ lecture-code.cpp
@@ -10,21 +10,29 @@ using std::cerr;
 using std::endl;
 
 int main(int argc, char* argv[]){
-	const size_t SIZE = 4;
-	Object* myObjects[SIZE]; // What is the type of myObjects
+	if (argc != 2){
+		cerr << "Not enough parameters, give me the array size!" << endl;
+		cerr << "Usage: " << endl;
+		cerr << argv[0] << " size" << endl;
+		return 1;
+	}
+	Object** myObjects = nullptr;
+	size_t size = atoi(argv[1]);
+	myObjects = new Object*[size];
 
 	myObjects[0] = new Person("Jake",2);
 	myObjects[1] = new Integer(3);
 	myObjects[2] = new Integer(7);
 	myObjects[3] = new Person("Blanca", 44);
 
-	for (int i = 0; i < SIZE; ++i) {
+	for (int i = 0; i < size; ++i) {
 		cout << myObjects[i]->ToString() << endl;
 	}
 
-	for (int i = 0; i < SIZE; ++i) {
+	for (int i = 0; i < size; ++i) {
 		delete myObjects[i];
 	}
+	delete[] myObjects;
 
 	return 0;
 }
