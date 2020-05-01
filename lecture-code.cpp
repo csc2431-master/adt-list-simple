c5d5b0532682e42c2ed57939a00d9f35efeeea5b
Carlos R. Arias <arias@spu.edu>
2020-05-01 09:53:21 -0700

    Array of Object* with memory leak

--- lecture-code.cpp
+++ lecture-code.cpp
@@ -11,16 +11,17 @@ using std::endl;
 
 int main(int argc, char* argv[]){
 	const size_t SIZE = 4;
-	Object myObjects[SIZE]; // What is the type of myObjects
+	Object* myObjects[SIZE]; // What is the type of myObjects
 
-	myObjects[0] = Person("Jake",2);
-	myObjects[1] = Integer(3);
-	myObjects[2] = Integer(7);
-	myObjects[3] = Person("Blanca", 44);
+	myObjects[0] = new Person("Jake",2);
+	myObjects[1] = new Integer(3);
+	myObjects[2] = new Integer(7);
+	myObjects[3] = new Person("Blanca", 44);
 
 	for (int i = 0; i < SIZE; ++i) {
 		cout << myObjects[i].ToString() << endl;
 	}
+
 
 	return 0;
 }
